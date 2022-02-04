4480cf06154db3ac4b035bf474a27c59613fade3
Ral Andrew <randrew934@outlook.com>
2022-02-04 14:10:56 +0100

    Change 1.0

---
+++ DEWS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DEWS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
